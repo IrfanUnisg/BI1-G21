35d5302c7671701627215d7c4cebfa98c81ae37a
Irfan Kujovic <irfan.kujovic@unisg.ch>
2024-10-15 19:28:39 +0200

    Update Tracking.py

--- pages/Tracking.py
+++ pages/Tracking.py
@@ -1,3 +1,4 @@
 import streamlit as st
+stromverbrauch = st.session_state.get("stromverbrauch", "Nicht verfügbar")
 st.subheader("Stromverbrauch")
 st.write(f"Ihr aktueller Stromverbrauch: {stromverbrauch} kWh")

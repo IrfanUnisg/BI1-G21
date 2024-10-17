225151d1c3ccda7bdf3bda5f9f0d308c628671be
Irfan Kujovic <irfan.kujovic@unisg.ch>
2024-10-17 20:53:02 +0200

    Update Information.py

--- pages/Information.py
+++ pages/Information.py
@@ -3,7 +3,7 @@ st.header(Information)
 
 
 st.text(Wussten Sie schon, dass Solaranlagen dafür genutzt werden können, weit mehr zu leisten als nur Strom für Ihr Zuhause zu erzeugen? Moderne Solartechnologien eröffnen zahlreiche zusätzliche Nutzungsmöglichkeiten, die viele Hausbesitzer nicht kennen.
-  Zum Beispiel können Sie mit solarbetriebenen Wasserpumpen in Ihrem Garten effizienter bewässern oder sogar einen Brunnen versorgen – ganz ohne zusätzlichen Energieaufwand. Haben Sie einen Pool? Solaranlagen können auch genutzt werden, um Ihr Schwimmbad umweltfreundlich zu beheizen.
+  Zum Beispiel können Sie mit solarbetriebenen Wasserpumpen in Ihrem Garten effizienter bewässern oder sogar einen Brunnen versorgen - ganz ohne zusätzlichen Energieaufwand. Haben Sie einen Pool? Solaranlagen können auch genutzt werden, um Ihr Schwimmbad umweltfreundlich zu beheizen.
   Für Ihr Zuhause bieten sich außerdem solare Luftaufbereitungssysteme an, die für ein angenehmes Raumklima sorgen und dabei weniger Energie verbrauchen als herkömmliche Klimaanlagen. Wussten Sie, dass Ihre Fenster und Fassaden durch die Installation transparenter Solarpanels auch Strom erzeugen könnten, ohne dass sie dabei Licht und Sicht beeinträchtigen?
   Eine weitere innovative Anwendung sind solarbetriebene Außenbeleuchtungen. Sie können ganz einfach eigenständige Garten- oder Straßenleuchten installieren, die tagsüber Sonnenenergie sammeln und nachts automatisch leuchten – ohne aufwendige Verkabelung und zusätzliche Stromkosten.
   Auch für die kalten Monate gibt es einen Vorteil: Solaranlagen können in Kombination mit solaren Heizungssystemen die Kosten für die Raumwärme erheblich senken.

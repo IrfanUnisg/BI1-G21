acf4d3748f62ada49714b29a21e8aaa43dc94719
Irfan Kujovic <irfan.kujovic@unisg.ch>
2024-10-18 21:20:33 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,15 +1,6 @@
 import streamlit as st
 import plotly.graph_objects as go
-st.markdown(
-    """
-    <style>
-    .css-1d391kg {
-        background-color: #facb04; /* Change this to your desired color */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
 # Seite konfigurieren (must be first command)
 st.set_page_config(page_title="Virtual Battery", page_icon="⚡", layout="wide")
 
@@ -32,19 +23,19 @@ guthaben = st.session_state["guthaben"]
 cash = st.session_state["cash"]
 kapazitaet = st.session_state["kapazitaet"]
 
-
-st.markdown(f"<h1 style='color:{blue_color};'>Virtual Battery</h1>", unsafe_allow_html=True)
+# Titel
+st.markdown("<h1>Virtual Battery</h1>", unsafe_allow_html=True)
 
 # Kontoübersicht (Stromguthaben und Cash)
-st.markdown(f"<h2 style='color:{blue_color};'>Stromkonto</h2>", unsafe_allow_html=True)
+st.markdown("<h2>Stromkonto</h2>", unsafe_allow_html=True)
 col3, col4 = st.columns(2)
 with col3:
-    st.markdown(f"<p style='color:{blue_color};'>Ihr aktuelles Stromguthaben: {guthaben} kWh</p>", unsafe_allow_html=True)
+    st.markdown(f"<p>Ihr aktuelles Stromguthaben: {guthaben} kWh</p>", unsafe_allow_html=True)
 with col4:
-    st.markdown(f"<p style='color:{blue_color};'>Ihr Kontoguthaben: {cash:.2f} CHF</p>", unsafe_allow_html=True)
+    st.markdown(f"<p>Ihr Kontoguthaben: {cash:.2f} CHF</p>", unsafe_allow_html=True)
 
 # Anzeige des Batterie-Status als Gauge-Diagramm
-st.markdown(f"<h2 style='color:{blue_color};'>Batterie-Status</h2>", unsafe_allow_html=True)
+st.markdown("<h2>Batterie-Status</h2>", unsafe_allow_html=True)
 fig = go.Figure(go.Indicator(
     mode="gauge+number",
     value=guthaben,
@@ -55,15 +46,15 @@ fig = go.Figure(go.Indicator(
 st.plotly_chart(fig, use_container_width=True)
 
 # Stromhandel
-st.markdown(f"<h2 style='color:{blue_color};'>Stromhandel</h2>", unsafe_allow_html=True)
+st.markdown("<h2>Stromhandel</h2>", unsafe_allow_html=True)
 col5, col6 = st.columns(2)
 with col5:
     trade_type = st.radio("Möchten Sie Strom kaufen oder verkaufen?", ("Kaufen", "Verkaufen"))
     trade_amount = st.number_input(f"Menge an Strom zum {trade_type.lower()} (kWh)", min_value=0)
 
 with col6:
-    st.markdown(f"<p style='color:{blue_color};'>Kaufpreis: {preis_kauf * 100:.2f} Rp/kWh</p>", unsafe_allow_html=True)
-    st.markdown(f"<p style='color:{blue_color};'>Verkaufspreis: {preis_verkauf * 100:.2f} Rp/kWh</p>", unsafe_allow_html=True)
+    st.markdown(f"<p>Kaufpreis: {preis_kauf * 100:.2f} Rp/kWh</p>", unsafe_allow_html=True)
+    st.markdown(f"<p>Verkaufspreis: {preis_verkauf * 100:.2f} Rp/kWh</p>", unsafe_allow_html=True)
 
 if st.button(f"{trade_type} bestätigen"):
     total_price = trade_amount * (preis_kauf if trade_type == "Kaufen" else preis_verkauf)

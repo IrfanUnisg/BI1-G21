4c3c413e78e1d83f6513a3d562cec3bc282210cf
Irfan Kujovic <irfan.kujovic@unisg.ch>
2024-10-22 20:37:47 +0200

    Update 5 Ranking.py

--- pages/5 Ranking.py
+++ pages/5 Ranking.py
@@ -6,17 +6,20 @@ st.set_page_config(page_title="Ranking", page_icon="⚡", layout="centered")
 
 # Fiktive Daten für Rankings
 data_schweiz = {
+    "Rang": [1, 2, 3, 4, 5],
     "Name": ["Lukas Meier", "Sarah Müller", "Thomas Schmid", "Claudia Huber", "Michael Weber"],
     "Blitze": [120, 115, 110, 105, 100]
 }
 
 data_gemeinde = {
+    "Rang": [1, 2, 3, 4, 5],
     "Name": ["Anna Steiner", "Jonas Keller", "Laura Frei", "Peter Graf", "Monika Berger"],
     "Blitze": [90, 85, 80, 75, 70]
 }
 
 data_freunde = {
-    "Name": ["Paul Fischer", "Nina Maurer", "David Baumann", "Katja Beck", "Chris Vogel"],
+    "Rang": [1, 2, 3, 4, 5],
+    "Name": ["Luc Fischer", "Irfan Kujovic", "Celia Bührer", "Marco Köhler", "Flurin Oehler"],
     "Blitze": [65, 60, 55, 50, 45]
 }
 
@@ -26,23 +29,23 @@ df_gemeinde = pd.DataFrame(data_gemeinde)
 df_freunde = pd.DataFrame(data_freunde)
 
 # Seite Titel
-st.markdown("<h1 style='text-align: center;'>⚡ Ranking ⚡</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>⚡ Ranking </h1>", unsafe_allow_html=True)
 st.markdown("---")
 
 # Ranking Schweiz
-st.markdown("<h2 style='text-align: center;'>Ranking: Schweiz</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'>Schweiz</h2>", unsafe_allow_html=True)
 st.table(df_schweiz)
 
 st.markdown("---")
 
 # Ranking Gemeinde
-st.markdown("<h2 style='text-align: center;'>Ranking: Gemeinde</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'>Gemeinde Uzwil</h2>", unsafe_allow_html=True)
 st.table(df_gemeinde)
 
 st.markdown("---")
 
 # Ranking Freunde
-st.markdown("<h2 style='text-align: center;'>Ranking: Freunde</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'>Freunde</h2>", unsafe_allow_html=True)
 st.table(df_freunde)
 
 # Abschluss

059f2b28a3f19846cc7ce3022386abfe4397ac03
Irfan Kujovic <irfan.kujovic@unisg.ch>
2024-10-23 00:44:40 +0200

    Update 6 Information.py

--- pages/6 Information.py
+++ pages/6 Information.py
@@ -2,41 +2,41 @@ import streamlit as st
 st.set_page_config(layout="wide")
 # Einführungstext
 st.header("Wussten Sie schon?")
-
+st.markdown("---")
 st.write("""
 Solaranlagen können weit mehr leisten als nur Strom für Ihr Zuhause zu erzeugen! Moderne Solartechnologien bieten viele zusätzliche Nutzungsmöglichkeiten, die Hausbesitzer oft noch nicht kennen. Hier sind einige interessante Anwendungsbeispiele:
 """)
-
+st.markdown("---")
 # Textabschnitt für solarbetriebene Wasserpumpen
 st.subheader("Solarbetriebene Wasserpumpen und Poolheizung")
 st.write("""
 Mit **solarbetriebenen Wasserpumpen** können Sie Ihren Garten effizient bewässern oder einen Brunnen betreiben – und das völlig autark vom Stromnetz. Wenn Sie einen Pool haben, können Sie ihn umweltfreundlich mit Solarenergie beheizen.
 """)
-
+st.markdown("---")
 # Textabschnitt für solare Luftaufbereitungssysteme
 st.subheader("Solare Luftaufbereitung und Energieeffizienz")
 st.write("""
 Auch **solare Luftaufbereitungssysteme** können für ein angenehmes Raumklima sorgen, dabei aber deutlich weniger Energie verbrauchen als herkömmliche Klimaanlagen. Ihre Fenster und Fassaden könnten durch transparente Solarpanels sogar Strom erzeugen, ohne dass Licht und Sicht beeinträchtigt werden.
 """)
-
+st.markdown("---")
 # Textabschnitt für solarbetriebene Außenbeleuchtung
 st.subheader("Solarbetriebene Außenbeleuchtung")
 st.write("""
 Installieren Sie eigenständige **solarbetriebene Garten- oder Straßenleuchten**, die tagsüber Sonnenenergie speichern und nachts automatisch leuchten – ganz ohne Verkabelung und zusätzliche Stromkosten.
 """)
-
+st.markdown("---")
 # Textabschnitt für solare Heizungssysteme
 st.subheader("Solare Heizungssysteme")
 st.write("""
 Für die kalten Monate sind **solare Heizungssysteme** ideal, um die Raumwärme auf umweltfreundliche Weise zu unterstützen und Ihre Heizkosten erheblich zu senken.
 """)
-
+st.markdown("---")
 # Textabschnitt für solare Warmwasser-Systeme
 st.subheader("Solarbetriebene Warmwassersysteme")
 st.write("""
 Solaranlagen können auch **Warmwasser** für den Haushalt bereitstellen, indem sie Sonnenenergie nutzen, um Wasser zu erwärmen. Dies kann besonders in den Sommermonaten erhebliche Energiekosteneinsparungen bringen.
 """)
-
+st.markdown("---")
 # Abschluss
 st.write("""
 Nutzen Sie das volle Potenzial Ihrer Solaranlage und gestalten Sie Ihr Zuhause effizienter und umweltfreundlicher!

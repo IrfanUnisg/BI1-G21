2f8fac0c5562c00c48781e8d82bd4271f22053b9
Irfan Kujovic <irfan.kujovic@unisg.ch>
2024-10-23 00:39:41 +0200

    Update 3 Live-Tracking.py

--- pages/3 Live-Tracking.py
+++ pages/3 Live-Tracking.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
-
+st.set_page_config(layout="wide")
 # Custom CSS for centering and stacking metric titles and values
 st.markdown("""
     <style>
